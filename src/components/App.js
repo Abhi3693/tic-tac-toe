69f30937666d812bc7f719ea457444a547cd9b35
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-04-05 12:06:05 +0530

    Added tic-tac-toe game useing react.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Header from './Header';
+import Game from './Game';
+function App() {
+  return (
+    <>
+      <Header />
+      <Game />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1 className='heading'>Tic Tac Toe</h1>
+    </header>
+  );
+}
+
+export default Header;
